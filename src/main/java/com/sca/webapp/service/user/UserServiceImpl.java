7a6269a83752634ae524e0298db4c9f2fd027aa9
Saikrishna <sairaju.jdev@gmail.com>
2021-11-27 13:48:41 +0530

    implementing Transactions in All Service Classes

--- src/main/java/com/sca/webapp/service/user/UserServiceImpl.java
+++ src/main/java/com/sca/webapp/service/user/UserServiceImpl.java
@@ -2,12 +2,16 @@ package com.sca.webapp.service.user;
 
 import com.sca.webapp.entity.User;
 import com.sca.webapp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
+@Slf4j
 public class UserServiceImpl implements UserService{
 
     @Autowired
@@ -15,12 +19,14 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public List<User> getAllUsers() {
+        log.info("Getting the All the Users : {}",repository.findAll());
 
         return repository.findAll();
     }
 
     @Override
     public User getUserById(int userId) {
+        log.info("Getting the User by UserId: {}",userId);
 
         return repository.findById(userId).get();
     }
